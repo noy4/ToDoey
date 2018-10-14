6e32e665764ba76cf43f493c55ba5220d54b0033
桑村直弥 <kuwamuranaoya@mbp.local>
2018-10-15 02:55:11 +0900

    Convert to MVC design pattern

---
+++ ToDoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  ToDoey
+//
+//  Created by 桑村直弥 on 2018/10/15.
+//  Copyright © 2018年 noy4. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+    
+}
